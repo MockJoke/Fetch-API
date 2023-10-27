322636bbed97336d60ebeccd5482b998d7b5cd1d
MockJoke <parvadesai404@gmail.com>
2023-10-27 12:53:16 +0530

    feat: Added the project

---
+++ app.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const tweetForm = document.getElementById("tweetForm");
+  const tweetText = document.getElementById("tweetText");
+  const responseDiv = document.getElementById("response");
+
+  tweetForm.addEventListener("submit", function (event) {
+    // to prevent the default behavior of event handlers on an HTML element (i.e. form submissions, links)
+    event.preventDefault();
+
+    const tweet = tweetText.value;
+    postTweet(tweet, responseDiv);
+  });
+});
+
+function postTweet(tweetContent, responseDiv) {
+  // Make an HTTP POST request to the backend
+  fetch("https://one00x-data-analysis.onrender.com/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ post: { content: tweetContent } }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json(); // Parse response as JSON
+      } else {
+        throw new Error("Tweet posting failed"); // Handle failure
+      }
+    })
+    .then((data) => {
+      // Handle successful tweet post
+      handleSuccessfulPost(data, responseDiv);
+    })
+    .catch((error) => {
+      // Handle error
+      handleErrorPost(error, responseDiv);
+    });
+}
+
+function handleSuccessfulPost(data, responseDiv) {
+  responseDiv.innerText = `Tweet posted successfully! Post ID: ${data.id}`;
+}
+
+function handleErrorPost(error, responseDiv) {
+  responseDiv.innerText = `Error: ${error.message}`;
+}
